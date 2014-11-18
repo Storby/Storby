d78c10cdea0428c3e044112c80aedbf899f3dcd5
Storby <vebrosok@hotmail.com>
2014-11-18 12:42:38 +0100

    Update 0.1

--- Storby/src/StorbyTest.java
+++ Storby/src/StorbyTest.java
@@ -4,7 +4,7 @@ import Utsende.HovedUtsende;
 public class StorbyTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hilsen til alle som er WIN TWINS");
+		System.out.println("Hilsen til alle som er #yoloswagnaice");
 		new HovedUtsende();
 	}
 
