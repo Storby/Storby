273f006b69baaace4c1bbefe65228dea1a70e672
Storby <vebrosok@hotmail.com>
2014-11-18 11:56:47 +0100

    #yolo

--- Storby/src/StorbyTest.java
+++ Storby/src/StorbyTest.java
@@ -4,7 +4,7 @@ import Utsende.HovedUtsende;
 public class StorbyTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hilsen til meg");
+		System.out.println("Hilsen til #yolo");
 		new HovedUtsende();
 	}
 
