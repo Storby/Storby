b1f28e7e2d4d25958311021c516c10fa454cc554
Storby <vebrosok@hotmail.com>
2014-11-18 10:55:19 +0100

    Endret Hovedmeny

--- Storby/src/Utsende/Hovedmeny.java
+++ Storby/src/Utsende/Hovedmeny.java
@@ -2,8 +2,12 @@ package Utsende;
 
 
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 
 
@@ -12,6 +16,27 @@ import javax.swing.JMenuBar;
 public class Hovedmeny extends JMenuBar {
 
 	public Hovedmeny() {
-		JMenu file = new JMenu("file");
+		//----------JMenuBar-----------\\
+				JMenuBar mbar = new JMenuBar();
+				
+				JMenu file = new JMenu("File");
+				mbar.add(file);
+				
+				JMenuItem exit = new JMenuItem("Exit");
+				file.add(exit);
+				
+				JMenu help = new JMenu("Help");
+				mbar.add(help);
+				
+				JMenuItem about = new JMenuItem("About");
+				help.add(about);
+				
+				class exitaction implements ActionListener{
+					public void actionPerformed (ActionEvent e ){
+						System.exit(0);
+					}
+				}
+				exit.addActionListener(new exitaction());
+				//------------------------------\\
 	}
 }
