15e816dd0c9c54ecf50f3f221382d359c742d62f
Thomas <cletustiberius@gmail.com>
2014-11-18 12:35:07 +0100

    wintwin

--- Storby/src/StorbyTest.java
+++ Storby/src/StorbyTest.java
@@ -4,7 +4,7 @@ import Utsende.HovedUtsende;
 public class StorbyTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hilsen til WIN TWIN");
+		System.out.println("Hilsen til alle som er WIN TWINS");
 		new HovedUtsende();
 	}
 
