a126e71cb5b81c3a458e5f00e5881ed50dc4f210
Storby <vebrosok@hotmail.com>
2014-11-20 15:14:45 +0100

    Beta version 1.1

--- Storby/src/StorbyTest.java
+++ Storby/src/StorbyTest.java
@@ -4,7 +4,7 @@ import Utsende.HovedUtsende;
 public class StorbyTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hilsen til alle som er awesome");
+		System.out.println("beta version 1.1");
 		new HovedUtsende();
 	}
 
