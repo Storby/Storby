c3b5f9c748f139a370aeedf41c9e7a351254ee10
Vebjørn Antoni <Vebjrn Antoni@Gaming-PC>
2014-12-06 15:51:16 +0100

    1.3
    - About
    	- added text

--- Storby/src/StorbyTest.java
+++ Storby/src/StorbyTest.java
@@ -4,7 +4,7 @@ import Utsende.HovedUtsende;
 public class StorbyTest {
 
 	public static void main(String[] args) {
-		System.out.println("beta version 1.2");
+		System.out.println("Beta version 1.3");
 		new HovedUtsende();
 	}
 
--- Storby/src/controller/aboutAction.java
+++ Storby/src/controller/aboutAction.java
@@ -1,19 +1,37 @@
 package controller;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class aboutAction implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
+		
+		//JLabel//
+		JLabel label = new JLabel(
+				"Vi er en student gruppe ved høyskolen i Vestfold og Buskerud. "
+				+ "Dette er våres eksamens oppgave i Java året 2014. "
+				+ "Denne teksten skulle egentlig fortsette under, men siden det er en feil. "
+				+ "Fortsetter den bare ut i ingenting :S "
+						
+				);
+		//JButton
+		JButton button = new JButton("Ok");
+		//Jpanel//
+		JPanel panel = new JPanel();
+		JPanel panel2 = new JPanel();
+		panel.add(label);
+		panel2.add(button);
+		//JFrame//
 		JFrame aboutFrame = new JFrame("Om oss");
 		aboutFrame.setVisible(true);
 		aboutFrame.setSize(400,200);
 		aboutFrame.setLocationRelativeTo(null);
-		
-
+		aboutFrame.add(panel, BorderLayout.WEST);
+		aboutFrame.add(panel2, BorderLayout.SOUTH);
 	}
 
 }
